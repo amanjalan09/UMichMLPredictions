cdf0f8fe78dae741ef775a291f8e823f96201759
Aman Srivastav <amansr@umich.edu>
2020-08-22 00:09:37 -0700

    added some template frontend code to modify, modified server to use global model, added requirements.txt for venv

--- server/server.py
+++ server/server.py
@@ -9,10 +9,13 @@ import math
 from model import *
 
 app = Flask(__name__)
-#cache = Cache(app, config={'CACHE_TYPE': 'simple',"CACHE_DEFAULT_TIMEOUT": 86400,'CACHE_THRESHOLD':math.inf})
+cache = Cache(app, config={'CACHE_TYPE': 'simple',"CACHE_DEFAULT_TIMEOUT": 86400,'CACHE_THRESHOLD':math.inf})
+
+f = open("model.pkl",'rb')
+model = pickle.load(f)
         
 @app.route("/",methods=["GET","POST"])
-#@cache.cached(timeout=86400, query_string=True)
+@cache.cached(timeout=86400, query_string=True)
 def home():
     if request.method == "GET":
         content = request.json
@@ -21,10 +24,7 @@ def home():
         #convert json to data list and pass into
         content = request.json
         data = [[element for key,element in content.items()]]
-        print(data)
         head = cleaning(data)
-        f = open("model.pkl",'rb')
-        model = pickle.load(f)
         result = model.predict(head)
         return jsonify({"result":int(result[0])})
 
